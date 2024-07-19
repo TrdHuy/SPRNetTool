4e3632a3a6157684225e15d785cbfaafea9407fc
Tran Huy <trdtranduchuy@gmail.com>
2024-07-20 00:18:13 +0700

    [#12] Triển khai code signing

---
+++ SignToolUtilForClient.ps1
@@ -0,0 +1,50 @@
+
+# FOR CSPROJ
+# <Target Name="ExecuteScript" AfterTargets="DownloadScript">
+#     <Exec Command="powershell -File '$(ProjectDir)\SignToolUtilForClient.ps1' -filesToSign '$(TargetDir)ArtWiz.exe,$(TargetDir)ArtWiz.dll'"/>
+# </Target>
+
+# FOR PSW
+# $filesToSign = "ArtWiz.exe,ArtWiz.dll"
+# .\SignToolUtilForClient.ps1 -filesToSign $filesToSign
+param (
+     [string]$filesToSign,
+     [string]$localConfigPath
+)
+$_TOKEN = $env:GITHUB_TOKEN
+$ISLOCAL = $env:ISLOCAL
+if (-not $ISLOCAL) {
+     $ISLOCAL = $true
+}
+ 
+Write-Host filesToSign: $filesToSign
+Write-Host localConfigPath: $localConfigPath
+
+if ($ISLOCAL -eq $true) {
+     Write-Host "Assign local variable"
+
+     $localXmlString = Get-Content -Raw -Path $localConfigPath
+	
+     # Tạo đối tượng XmlDocument và load chuỗi XML vào nó
+     $localXmlDoc = New-Object System.Xml.XmlDocument
+     $localXmlDoc.PreserveWhitespace = $true
+     $localXmlDoc.LoadXml($localXmlString)
+     $_TOKEN = $localXmlDoc.configuration.GITHUB_TOKEN
+
+}
+
+if (-not $_TOKEN) {
+     throw "GITHUB_TOKEN must not be null "
+}
+
+$headers = @{
+     Authorization = "token $_TOKEN"
+}
+$url = "https://raw.githubusercontent.com/TrdHuy/_TrdBuildPlugin/master/SignToolUtil.ps1"
+$script = Invoke-RestMethod -Uri $url -Headers $headers
+
+$modifiedScriptContent = "`$filesToSign = `"$filesToSign`"`n" `
+     + "`$TOKEN = `"$_TOKEN`"`n" `
+     + $script
+Write-Host Start signing internal
+Invoke-Expression $modifiedScriptContent
