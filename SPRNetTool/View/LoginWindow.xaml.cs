e5d2758e09c00609b172f4ee93e71f2c9fb0aae7
VuDucHihi <vuminhduc232323@gmail.com>
2025-01-09 05:37:15 +0700

    [#324] Hide Title Bar by Using DataTrigger

--- SPRNetTool/View/LoginWindow.xaml.cs
+++ SPRNetTool/View/LoginWindow.xaml.cs
@@ -2,6 +2,7 @@
 using ArtWiz.View.Widgets;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,35 @@ namespace ArtWiz.View
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class LoginWindow : BaseArtWizWindow
+    public partial class LoginWindow : BaseArtWizWindow, INotifyPropertyChanged
     {
+        private bool _isTitleBarHide;
+
+        public bool IsTitleBarHide
+        {
+            get => _isTitleBarHide;
+            set
+            {
+                if (_isTitleBarHide != value)
+                {
+                    _isTitleBarHide = value;
+                    OnPropertyChanged(nameof(IsTitleBarHide));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public LoginWindow()
         {
             InitializeComponent();
+            IsTitleBarHide = true;
+            this.DataContext = this;
         }
     }
     
