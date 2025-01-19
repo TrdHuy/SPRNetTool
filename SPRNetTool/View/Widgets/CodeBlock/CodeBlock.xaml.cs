3d2e36ce4829976e03cd84a2b7dda9af0518575e
TrdHuy <trdtranduchuy@gmail.com>
2025-01-19 21:57:07 +0700

    Implement code block feature p1

---
+++ SPRNetTool/View/Widgets/CodeBlock/CodeBlock.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArtWiz.View.Widgets.CodeBlock
+{
+    /// <summary>
+    /// Interaction logic for CodeBlock.xaml
+    /// </summary>
+    public partial class CodeBlock : VerticalVirtualizingPanel
+    {
+        public CodeBlock()
+        {
+            InitializeComponent();
+        }
+
+        public override Canvas PART_ContentCanvasContainer => ContentCanvasContainer;
+
+        public override Canvas PART_MainCanvasContainer => MainCanvasContainer;
+    }
+}
